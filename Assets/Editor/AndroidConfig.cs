861767eeb14fd49a1f3c16d5a4e32b8432c8843c
Synergy Bangladesh <synergybangladesh.info@gmail.com>
2026-07-27 17:09:33 +0600

    Add files via upload

--- Assets/Editor/AndroidConfig.cs
+++ Assets/Editor/AndroidConfig.cs
@@ -20,8 +20,8 @@ namespace TankBattle.EditorTools
             PlayerSettings.productName = "Tank Battle LAN";
             PlayerSettings.companyName = "TankBattleLAN";
             PlayerSettings.SetApplicationIdentifier(NamedBuildTarget.Android, "com.tankbattlelan.game");
-            PlayerSettings.bundleVersion = "2.7.0";
-            PlayerSettings.Android.bundleVersionCode = 9;
+            PlayerSettings.bundleVersion = "2.8.0";
+            PlayerSettings.Android.bundleVersionCode = 10;
 
             // Scripting: IL2CPP release for both mainstream ABIs.
             PlayerSettings.SetScriptingBackend(NamedBuildTarget.Android, ScriptingImplementation.IL2CPP);
